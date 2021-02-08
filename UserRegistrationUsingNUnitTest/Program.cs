f24f4bc1fcab3cfc5f0cfa6b3b41d457d03095d5
diliprathod32 <diliprathod32@gmail.com>
2021-02-09 01:58:40 +0530

    [Dilip] PassWordRule2ShouldHaveAtlease1UpperCharacter

--- UserRegistrationUsingNUnitTest/Program.cs
+++ UserRegistrationUsingNUnitTest/Program.cs
@@ -9,7 +9,8 @@ namespace UserRegistrationUsingNUnitTest
         Regex lastNameregex = new Regex("^[A-Z][a-zA-Z]{2,}$");
         Regex emailregex = new Regex("^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}(.[][a-zA-Z]{2})*$");
         Regex mobileNumberregex = new Regex(@"^[0-9]{2,}\s[0-9]{10,}$");
-        Regex PasswordRule1regex = new Regex(@"^.{8,}$");
+        Regex passwordRule1regex = new Regex(@"^.{8,}$");
+        Regex passwordRule2regex = new Regex(@"^(?=.*[A-Z]).{8,}$");
         public bool FirstNameRegex(string firstname)
         {
             return (firstNameregex.IsMatch(firstname)); 
@@ -27,9 +28,13 @@ namespace UserRegistrationUsingNUnitTest
         {
             return (mobileNumberregex.IsMatch(mobileNumber));
         }
-        public bool PasswordRule1Regex(string mobileNumber)
+        public bool PasswordRule1Regex(string passwordRule1)
         {
-            return (PasswordRule1regex.IsMatch(mobileNumber));
+            return (passwordRule1regex.IsMatch(passwordRule1));
+        }
+        public bool PasswordRule2Regex(string passwordRule2)
+        {
+            return (passwordRule2regex.IsMatch(passwordRule2));
         }
         static void Main(string[] args)
         {
